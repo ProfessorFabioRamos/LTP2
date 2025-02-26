9379c44a10149e9a6f12ea3dac9fc12b5aa20d67
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-02-26 09:42:43 -0300

    Update composicao.py

--- Associacao/composicao.py
+++ Associacao/composicao.py
@@ -7,4 +7,11 @@ class Carro:
     def __init__(self,marca,modelo,tipo_motor):
         self.marca = marca
         self.modelo = modelo
-        self.tipo
+        self.tipo_motor = Motor(tipo_motor) #Composição: o motor pertence
+                                            #exlusivamete ao carro
+    def exibirInfo(self):
+        print(f'Carro {self.marca} {self.modelo} - Motor {self.motor.tipo}')
+
+#Instanciação do Objeto
+carro1 = Carro('Lamborghini','Aventador', 'V12')
+carro1.exibirInfo()
