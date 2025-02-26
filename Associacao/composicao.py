7db2addefbb163dc8405da3235c3e7c1e55efe2a
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-02-26 09:42:06 -0300

    Add files via upload

---
+++ Associacao/composicao.py
@@ -0,0 +1,10 @@
+#Composição
+class Motor:
+    def __init__(self,tipo):
+        self.tipo
+
+class Carro:
+    def __init__(self,marca,modelo,tipo_motor):
+        self.marca = marca
+        self.modelo = modelo
+        self.tipo
