d385c8a83b572e1e90bc9e13f9145f2efabfebaa
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-02-28 08:43:16 -0300

    Update associacao.py

--- Associacao/associacao.py
+++ Associacao/associacao.py
@@ -1,17 +1,21 @@
-#Associação Simples
+#ASSOCIAÇÃO SIMPLES
+#Quando uma classe usa outra sem estabelecer 
+#uma dependência forte.
+#O Aluno contém uma referência ao Curso,
+#mas o Curso pode existir sem o Aluno
 class Curso:
     def __init__(self,nome):
         self.nome = nome
 
 class Aluno:
-    def __init__(self, nome, curso):
+    def __init__(self,nome,curso):
         self.nome = nome
-        self.curso = curso  #Associação Simples
-    
-    def info(self):
-        print(f'Aluno:{self.nome} - Curso:{self.curso.nome}')
+        self.curso = curso      #ASSOCIAÇÃO SIMPLES  
 
-#Instanciando objetos
-curso1 = Curso("Ciência da Computação")
+    def exibirInfo(self):
+        print(f'Aluno:{self.nome} - Curso: {self.curso.nome}')
+
+#Instanciando Objetos
+curso1 = Curso('Ciência da Computação')
 aluno1 = Aluno('Bob',curso1)
-aluno1.info()
+aluno1.exibirInfo()
