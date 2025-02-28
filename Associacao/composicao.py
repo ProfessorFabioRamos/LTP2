9c785edf408220a1809b3bd210e99e217545b205
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-02-28 09:38:15 -0300

    Update composicao.py

--- Associacao/composicao.py
+++ Associacao/composicao.py
@@ -1,17 +1,19 @@
-#Composição
-class Motor:
+#COMPOSIÇÃO
+#Um objeto contém outro e não faz sentido existir sem ele.
+#O Motor é criado dentro do Carro, e não pode existir sem ele.
+class Motor():
     def __init__(self,tipo):
-        self.tipo
+        self.tipo = tipo
 
 class Carro:
     def __init__(self,marca,modelo,tipo_motor):
         self.marca = marca
         self.modelo = modelo
-        self.tipo_motor = Motor(tipo_motor) #Composição: o motor pertence
-                                            #exlusivamete ao carro
+        self.motor = Motor(tipo_motor)  #Composição: o motor pertence 
+                                        #exclusivamente ao carro
     def exibirInfo(self):
-        print(f'Carro {self.marca} {self.modelo} - Motor {self.motor.tipo}')
+        print(f'Carro {self.marca},{self.modelo}, - Motor: {self.motor.tipo}')
 
-#Instanciação do Objeto
-carro1 = Carro('Lamborghini','Aventador', 'V12')
+#Instanciando objeto
+carro1 = Carro('Lamborghini','Aventador','V12')
 carro1.exibirInfo()
