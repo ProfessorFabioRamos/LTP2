a15a153deb6e3f6b34a6ca7604ad4d53fcd581ac
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-03-28 10:53:02 -0300

    Add files via upload

---
+++ Python_DB/UPDATE_EXEMPLOS.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+conexao = sqlite3.connect('empresa.db')
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS pedidos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cliente_id INTEGER NOT NULL,
+    valor REAL CHECK(valor>0),
+    status TEXT CHECK (status IN ('pendente','concluido','cancelado'))
+)
+""")
+def inserir_pedidos(cliente_id,valor,status):
+    cursor.execute("INSERT INTO pedidos (cliente_id,valor,status) VALUES(?,?,?)",
+    (cliente_id,valor,status))
+
+#inserir_pedidos(22, 400.50,'pendente')
+#inserir_pedidos(22, 28.50,'concluido')
+#inserir_pedidos(14, 39.50,'concluido')
+#inserir_pedidos(42, 1000.00,'cancelado')
+
+#cursor.execute("UPDATE pedidos SET status = 'concluido' WHERE id = 1") # ATUALIZAR UM CAMPO PELO ID
+# ATUALIZAR MULTIPLOS CAMPOS
+#cursor.execute("UPDATE pedidos SET valor = 33.33, status = 'pendente' WHERE id = 2")
+# ATUALIZA MULTIPLOS LINHAS COM A MESMA CONDIÇÃO
+cursor.execute("UPDATE pedidos SET status = 'pendente' WHERE status = 'concluido'")
+# ATUALIZAR UM CAMPO POR OUTRO VALOR
+cursor.execute("UPDATE pedidos SET status = 'concluido' WHERE cliente_id = 22") 
+conexao.commit()
+conexao.close()
