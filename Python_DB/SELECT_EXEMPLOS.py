518f1a6f2dc6cc593466adb5b416536bd061ecfd
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-04-02 09:30:05 -0300

    Update SELECT_EXEMPLOS.py

--- Python_DB/SELECT_EXEMPLOS.py
+++ Python_DB/SELECT_EXEMPLOS.py
@@ -18,23 +18,26 @@ CREATE TABLE IF NOT EXISTS clientes(
     date_time_conta TIMESTAMP
 )
 """)
+
 def inserir_cliente(n,i,e,d):
     cursor.execute("INSERT INTO clientes (nome,idade,email,date_time_conta) VALUES(?,?,?,?)",
         (n,i,e,d))
     
-inserir_cliente('Joao',45,'joao@email.com',data_hora_local)
-inserir_cliente('Helena',16,'helena@email.com',data_hora_local)
-inserir_cliente('Renata',16,'renata@email.com',data_hora_local)
-inserir_cliente('Pedro',45,'pedro@email.com',data_hora_local)
-inserir_cliente('Manuel',30,'manuel@email.com',data_hora_local)
-inserir_cliente('Carla',41,'carla@email.com',data_hora_local)
+# inserir_cliente('Kyle', 16, 'kyle@gmail.com', data_hora_local)
+# inserir_cliente('Helena', 16, 'helena@hotmail.com', data_hora_local)
+# inserir_cliente('Manuel', 45, 'manuel@gmail.com', data_hora_local)
+# inserir_cliente('Cartman', 23, 'cartman@gmail.com', data_hora_local)
+# inserir_cliente('Alice', 45, 'alice@email.com', data_hora_local)
+# inserir_cliente('Jenny', 34, 'jenny@email.com', data_hora_local)
 
 #cursor.execute("SELECT * FROM clientes") # SELECIONA TODOS OS REGISTROS
-#cursor.execute("SELECT nome, date_time_conta FROM clientes") #APENAS COLUNAS ESPECIFICAS
+#cursor.execute("SELECT nome, idade FROM clientes") #APENAS COLUNAS ESPECIFICAS
 #cursor.execute("SELECT * FROM clientes ORDER BY idade ASC") # ORDENA RESULTADOS (DESC-decrescente)
+#cursor.execute("SELECT * FROM clientes ORDER BY nome DESC") # ORDENA RESULTADOS (DESC-decrescente)
 #cursor.execute("SELECT * FROM clientes WHERE idade > 30") # FILTRO DE RESULTADOS
-#cursor.execute("SELECT * FROM clientes WHERE idade > 30 AND email LIKE '%@email.com'") # FILTRO DE RESULTADOS
-#cursor.execute("SELECT * FROM clientes LIMIT 1") #LIMITANDO OS RESULTADOS
+#cursor.execute("SELECT * FROM clientes WHERE email LIKE '%@gmail.com'") # FILTRO DE RESULTADOS COM FINAL ESPECIFICO
+#cursor.execute("SELECT * FROM clientes WHERE idade > 18 AND email LIKE '%@gmail.com'") # FILTRO DE RESULTADOS COM FINAL ESPECIFICO
+#cursor.execute("SELECT * FROM clientes LIMIT 2") #LIMITANDO OS RESULTADOS
 #cursor.execute("SELECT * FROM clientes ORDER BY idade ASC LIMIT 1") #LIMITANDO OS RESULTADOS POR ORDEM
 cursor.execute("SELECT idade, COUNT(*) FROM clientes GROUP BY idade") #AGRUPANDO RESULTADOS
 
