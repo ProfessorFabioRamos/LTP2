da43613475c5767fbe81c210121084568767c947
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-04-02 10:28:39 -0300

    Update UPDATE_EXEMPLOS.py

--- Python_DB/UPDATE_EXEMPLOS.py
+++ Python_DB/UPDATE_EXEMPLOS.py
@@ -1,5 +1,4 @@
 import sqlite3
-
 conexao = sqlite3.connect('empresa.db')
 cursor = conexao.cursor()
 
@@ -11,21 +10,30 @@ CREATE TABLE IF NOT EXISTS pedidos(
     status TEXT CHECK (status IN ('pendente','concluido','cancelado'))
 )
 """)
-def inserir_pedidos(cliente_id,valor,status):
+
+def inserir_pedidos(c,v,s):
     cursor.execute("INSERT INTO pedidos (cliente_id,valor,status) VALUES(?,?,?)",
-    (cliente_id,valor,status))
+    (c,v,s))
 
 #inserir_pedidos(22, 400.50,'pendente')
 #inserir_pedidos(22, 28.50,'concluido')
 #inserir_pedidos(14, 39.50,'concluido')
 #inserir_pedidos(42, 1000.00,'cancelado')
 
-#cursor.execute("UPDATE pedidos SET status = 'concluido' WHERE id = 1") # ATUALIZAR UM CAMPO PELO ID
+# ATUALIZAR UM CAMPO PELO ID
+#cursor.execute("UPDATE pedidos SET status = 'concluido' WHERE id = 1") 
+
 # ATUALIZAR MULTIPLOS CAMPOS
 #cursor.execute("UPDATE pedidos SET valor = 33.33, status = 'pendente' WHERE id = 2")
+
 # ATUALIZA MULTIPLOS LINHAS COM A MESMA CONDIÇÃO
 cursor.execute("UPDATE pedidos SET status = 'pendente' WHERE status = 'concluido'")
+
 # ATUALIZAR UM CAMPO POR OUTRO VALOR
-cursor.execute("UPDATE pedidos SET status = 'concluido' WHERE cliente_id = 22") 
+cursor.execute("UPDATE pedidos SET status = 'cancelado' WHERE cliente_id = 22") 
+
+# ATUALIZAR COM  UMA CONDIÇÃO
+cursor.execute("UPDATE pedidos SET status = 'concluido' WHERE valor >100") 
+
 conexao.commit()
 conexao.close()
