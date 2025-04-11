8288006652eaf013282a4082915983ca917ba49a
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-04-11 09:14:33 -0300

    Update CASOS_TESTE_INSERT.py

--- Python_DB/CASOS_TESTE_INSERT.py
+++ Python_DB/CASOS_TESTE_INSERT.py
@@ -1,6 +1,7 @@
+# Casos de Teste
 import sqlite3
 
-conexao = sqlite3.connect("Inventario.db")
+conexao = sqlite3.connect('empresa.db')
 cursor = conexao.cursor()
 
 cursor.execute("""
@@ -12,35 +13,35 @@ CREATE TABLE IF NOT EXISTS itens(
     )
 """)
 
-# CREATE - Inserir item
-def inserir_item(nome,tipo,valor):
+def inserir_itens(nome, tipo,valor):
     # VALIDAÇÕES
-    if not isinstance(nome,str):
-        print('Name com tipo incorreto!')
+    #print(type(nome))
+    #VALIDAÇÃO DE STRING
+    if not isinstance(nome, str):
+        print("Name com tipo incorreto")
         return
-    if not isinstance(valor,int):
-        print('Valor com tipo incorreto!')
-        return 
-    
+    #VALIDAÇÃO DE INTEIRO
+    if not isinstance(valor, int):
+        print("Valor com tipo incorreto")
+        return
     try:
-        cursor.execute("INSERT INTO itens (nome,tipo,valor) VALUES(?,?,?)", 
-                       (nome,tipo,valor))
+        cursor.execute("INSERT INTO itens(nome, tipo,valor) VALUES(?,?,?)",
+                    (nome, tipo,valor))
         conexao.commit()
-        print("Item inserido com sucesso")
+        print("Item inserido com sucesso!")
     except sqlite3.IntegrityError as e:
-        print("Erro: ", e)
+        print("Erro:",e)
 
-##################### CASOS DE TESTE DA FUNÇÃO INSERIR ##########################################
-
-# #Teste comum e válido
-# inserir_item('Espada Vorpal', 'Arma', 1000)
-# # Teste com item já existente
-# inserir_item('Espada Vorpal', 'Arma', 2000)
-# # Teste com valor negativo
-# inserir_item('Poção', 'Cura', -100)
-# # Teste com tipo incorreto
-# inserir_item(6000, 'Arma', 1000)
-# # Teste com tipo incorreto
-# inserir_item('Escudo de Ferro', 'Arma', '1000')
+##################### CASOS DE TESTE DA FUNÇÃO INSERIR ###########################
+#TESTE POSITIVO
+inserir_itens("Espada de Aço","Arma",500)
+# TESTE NEGATIVO COM ITEM JÁ EXISTENTE
+inserir_itens("Espada de Aço","Arma",500)
+# TESTE NEGATIVO COM VALOR NÃO PERMITIDO
+inserir_itens("Espada de Ouro","Arma",-800)
+# TESTE NEGATIVO COM TIPO INCORRETO (INT)
+inserir_itens(5000,"Arma",800)
+# TESTE NEGATIVO COM TIPO INCORRETO (STR)
+inserir_itens("Poção Menor","Cura",'20')
 
 conexao.close()
