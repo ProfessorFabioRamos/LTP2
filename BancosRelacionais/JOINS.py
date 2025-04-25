7df9fda57dddcee8dc3350eef5be3444ff7a7364
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-04-25 10:11:36 -0300

    Update JOINS.py

--- BancosRelacionais/JOINS.py
+++ BancosRelacionais/JOINS.py
@@ -18,29 +18,36 @@ CREATE TABLE IF NOT EXISTS pedidos(
     )
 """)
 
-cursor.execute("INSERT INTO clientes(nome) VALUES('Eddy'),('Martha'),('Mark')")
-cursor.execute("INSERT INTO pedidos(cliente_id, valor) VALUES(1,200.50),(1,150.00),(2,300.00),(4,40.50)")
-
-#JOIN COM INNER -> REGISTROS QUE CASAM NAS DUAS TABELAS 
-cursor.execute("""
-    SELECT clientes.nome, pedidos.valor 
-    FROM pedidos
-    INNER JOIN clientes ON pedidos.cliente_id = clientes.id
-""")
-
-# JOIN COM LEFT -> TODOS OS REGISTROS DA TABELA DA ESQUERDA MESMO QUE NÃO CASEM COM A DIREITA
-#cursor.execute("""
-    #SELECT clientes.nome, pedidos.valor 
-    #FROM pedidos
-    #LEFT JOIN clientes ON pedidos.cliente_id = clientes.id
-#""")
-
-# JOIN COM RIGHT -> TODOS OS REGISTROS DA TABELA DA DIREITA MESMO QUE NÃO CASEM COM A ESQUERDA
-# cursor.execute("""
-#     SELECT clientes.nome, pedidos.valor 
-#     FROM pedidos
-#     RIGHT JOIN clientes ON pedidos.cliente_id = clientes.id
+# cursor.execute("INSERT INTO clientes(nome) VALUES('Eddy'),('Martha'),('Mark')")
+# cursor.execute("""INSERT INTO pedidos(cliente_id, valor) VALUES
+#                (1,200.50),
+#                (1,150.00),
+#                (2,300.00),
+#                (4,40.50)
 # """)
+
+# JOIN COM INNER -> REGISTROS QUE CASAM NAS DUAS TABELAS
+# cursor.execute("""
+#     SELECT clientes.nome, pedidos.valor
+#     FROM pedidos
+#     INNER JOIN clientes ON pedidos.cliente_id = clientes.id
+# """)
+
+# JOIN COM LEFT -> TODOS OS REGISTROS DA TABELA DA ESQUERDA MESMO 
+# QUE NÃO TENHAM VALOR NA DIREITA
+# cursor.execute("""
+#     SELECT clientes.nome, pedidos.valor
+#     FROM pedidos
+#     LEFT JOIN clientes ON pedidos.cliente_id = clientes.id
+# """)
+
+# JOIN COM RIGHT -> TODOS OS REGISTROS DA TABELA DA DIREITA MESMO 
+# QUE NÃO TENHAM VALOR NA ESQUERDA
+cursor.execute("""
+    SELECT clientes.nome, pedidos.valor
+    FROM pedidos
+    RIGHT JOIN clientes ON pedidos.cliente_id = clientes.id
+""")
 
 for i in cursor.fetchall():
     print(i)
