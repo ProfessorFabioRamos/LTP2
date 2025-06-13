dae515a568ff06ce0d3a8304dac9ab6799727a82
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-06-13 09:13:10 -0300

    Update calculadora.py

--- PyQt/calculadora.py
+++ PyQt/calculadora.py
@@ -3,15 +3,31 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QLineEdit,
     QPushButton, QVBoxLayout, QHBoxLayout
 )
+from PyQt5.QtGui import QFont, QPalette, QColor
 
 class SomaApp(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Calculadora de Soma")
+        #tamanho da tela
+        self.resize(300,150)
+        #fonte padrão
+        fonte_padrao = QFont("Comic Sans MS",10)
+        #cor do fundo
+        self.setAutoFillBackground(True)
+        paleta = self.palette()
+        paleta.setColor(QPalette.Window, QColor("#4287f5"))
+        self.setPalette(paleta)
+
         # Criação dos widgets
         self.label1 = QLabel("Número 1:")
+        self.label1.setFont(fonte_padrao)
+        self.label1.setStyleSheet("color: orange; background-color:white;")
+
         self.input1 = QLineEdit()
+        self.input1.setStyleSheet("color: white; background-color:#000000;")
         self.label2 = QLabel("Número 2:")
+        self.label2.setFont(fonte_padrao)
         self.input2 = QLineEdit()
 
         self.botao = QPushButton("Somar")
@@ -19,7 +35,7 @@ class SomaApp(QWidget):
         # Conectar botão ao método de soma
         self.botao.clicked.connect(self.calcular_soma)
 
-        # Layouts
+         # Layouts
         layout = QVBoxLayout()
 
         linha1 = QHBoxLayout()
@@ -39,7 +55,7 @@ class SomaApp(QWidget):
         layout.addLayout(linha3)
 
         self.setLayout(layout)
-    
+
     def calcular_soma(self):
         try:
             num1 = float(self.input1.text())
@@ -49,8 +65,7 @@ class SomaApp(QWidget):
         except ValueError:
             self.resultado.setText("Insira números válidos")
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    janela = SomaApp()
-    janela.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+janela = SomaApp()
+janela.show()
+sys.exit(app.exec_())
